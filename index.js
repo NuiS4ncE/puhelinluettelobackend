c21ee946ae6c91689409c6cff7d0b837a25ee7d3
NuiS4ncE <nuis4nce@hotmail.com>
2020-02-09 23:48:19 +0200

    3.8, 3.9 and 3.10 done. 3.11 possibly done. due to git issues 3.8 and 3.9 code was deleted

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const path = require('path')
 
 app.use(express.json())
+app.use(express.static('build'))
 
 let persons = [
     {
@@ -29,6 +30,20 @@ let persons = [
     }
 ]
 const date = new Date()
+
+aapp.use(morgan('combined', {
+    stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+}))
+
+
+morgan.token('body', function (req, res) {
+    return JSON.stringify(req.body)
+})
+
+
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
+
+app.use(cors())
 
 app.get('/info', (req, res) => {
     res.send(`Phonebook has info for ${persons.length} people <br> <br> ${date}`)
@@ -93,15 +108,8 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
-app.use(morgan('combined', {
-    stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-}))
 
-app.use(morgan('tiny', {
-    function(req, res) { return res + req }
-}))
-
-const PORT = process.env.PORT || 3001 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+}) 
